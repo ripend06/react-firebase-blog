6eb9d4e7457d41ae9e4b11a461b73bb29b17016a
ripend06 <makoto6760@gmail.com>
2023-06-21 06:08:10 +0900

    feat:data-acquisition-firebase_Bc-Enhanced_230621

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import "./Home.css";
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ doc })));
+      // //console.log(data.docs.map((doc) => ({ ...doc })));
+      // console.log(data.docs.map((doc) => ({ ...doc.data()})));
+      // console.log(data.docs.map((doc) => ({ ...doc.data().author.username })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homePage">
+      {postList.map((post) => {
+        return (
+          <div className="postContents">
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postext}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+
+  );
+};
 
 export default Home
